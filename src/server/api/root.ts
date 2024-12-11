1d29c4651732ff3dde128cb2de286b591cc1d50c
Alex Lee <alex@numerade.com>
2024-12-11 14:01:20 -0800

    removed extraneous files

--- src/server/api/root.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from "@/server/api/trpc";
-import { userRouter } from "./routers/users";
-
-export const appRouter = createTRPCRouter({
-  user: userRouter,
-});
-
-export type AppRouter = typeof appRouter;
